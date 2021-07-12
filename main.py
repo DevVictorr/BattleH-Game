0271d62c1e6db216004354f2d688d8f815a79825
Bruno Antunes <brunoantunes4242@gmail.com>
2021-07-11 22:51:27 -0300

    News objs in main players

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from classes.Druid import Ximba as Druida
+from classes.Hunter import Elias as Hunter
+from classes.Mage import Strombf as Mage
+from classes.Warrior import Orcud as Warriors
 
 #Druid
-Druida_Victor = Druida.teste("Victor",50)
-Druida_Bruno = Druida.teste("Bruno",30)
+Druida_ginsumef = Druida.xim("Ginsumef", 535,123,316,278)
+Druida_eightica = Druida.xim("Eightica", 540,96,324,290)
 
 #Hunter
+Hunter_sethy = Hunter.eli("Sethy", 623,143,198,0)
+Hunter_micksp = Hunter.eli("Micksp", 587,139,183,0)
 
 #Mage
+Mage_caym = Mage.strom("Caym", 356,125,154,0)
+Mage_dicho = Mage.strom("Dicho", 319,143,203,0)
 
 #Warriors
+Warriors_anieba = Warriors.orc("Anieba", 750,134,320,0)
+Warriors_terthe = Warriors.orc("Terthe", 789,110,298,0)
 
 
+Druida_eightica.prt()
