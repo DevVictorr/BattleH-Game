7d3505199dc227a19a3d16eaa6c59ddbeebecd2a
Bruno Antunes <brunoantunes4242@gmail.com>
2021-07-11 22:57:13 -0300

    New attributes in Orcud

---
+++ classes/Warrior/Orcud.py
@@ -0,0 +1,10 @@
+from classes.Tree import *
+
+
+class orc(tree):
+
+    def __init__(self, name, life, atack, defense, heal):
+        super().__init__(name, life, atack, defense, heal)
+
+    def prt(self):
+        return super().prt()
